e13e1e76c320d0d64ad88bcb5cb06297711220fb
pawidachum <pawida.may@gmail.com>
2021-02-26 21:52:29 +0700

    master

---
+++ src/app/lab4_DirectivesTest/Lab4.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+     @Component({
+      selector: 'app-Lab4',
+      templateUrl: './Lab4.component.html',
+      styleUrls: ['./Lab4.component.css']
+     })
+     export class Lab4Component {
+        @Input()
+        title: string;
+        items = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
+      constructor() {}
+
+     }
