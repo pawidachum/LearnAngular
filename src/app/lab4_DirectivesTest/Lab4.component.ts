8535f0215a13cc7fb25df154542a4e141ca0bc70
pawidachum <pawida.may@gmail.com>
2021-02-26 22:43:17 +0700

    Lab4 complete test final

--- src/app/lab4_DirectivesTest/Lab4.component.ts
+++ src/app/lab4_DirectivesTest/Lab4.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
       styleUrls: ['./Lab4.component.css']
      })
      export class Lab4Component {
-        // @Input()
+        @Input()
         title: string;
         items = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
       constructor() {}
