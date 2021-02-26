46a9ade8dd97748b4f56604b784f4d6d703ab84a
pawidachum <pawida.may@gmail.com>
2021-02-26 21:52:00 +0700

    lab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './Home/home.component';
+import { FormsModule } from '@angular/forms';
+import { CountingComponent } from './counting/counting.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CountingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/counting/counting.component.ts
@@ -0,0 +1,15 @@
+ import { Component, Input } from '@angular/core';
+@Component({
+ selector: 'app-counting',
+ templateUrl: './counting.component.html',
+
+})
+export class CountingComponent {
+    @Input()
+    title: String;
+count = 0;
+ constructor() {}
+ countNumber() {
+   this.count = this.count + 1;
+}
+}
